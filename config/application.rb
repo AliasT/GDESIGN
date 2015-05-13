e5d3eaaf2db4737525fa445bae0ca23ab29d4ca9
aliasTTT <chaixiaobing7@gmail.com>
2015-05-13 11:07:26 +0800

    s

--- config/application.rb
+++ config/application.rb
@@ -28,21 +28,7 @@ module GDesign
     config.assets.logger = false
     config.middleware.use Rack::ContentLength
 
-    config.assets.precompile << Proc.new do |path|
-  if path =~ /\.(css|js)\z/
-    full_path = Rails.application.assets.resolve(path).to_path
-    app_assets_path = Rails.root.join('app', 'assets').to_path
-    if full_path.starts_with? app_assets_path
-      puts "including asset: " + full_path
-      true
-    else
-      puts "excluding asset: " + full_path
-      false
-    end
-  else
-    false
-  end
-end
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     
   end
 end

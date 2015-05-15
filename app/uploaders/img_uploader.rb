0a7111a35d9d51734de43eabd63e99d0100d8316
AliasT <AliasT@users.noreply.github.com>
2015-05-15 20:20:02 +0800

    Update img_uploader.rb

--- app/uploaders/img_uploader.rb
+++ app/uploaders/img_uploader.rb
@@ -13,7 +13,7 @@ class ImgUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/posts"
+    "upload/posts"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:

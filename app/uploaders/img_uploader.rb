908f0e01997410ab3c699fd777166898db04e787
aliasTTT <chaixiaobing7@gmail.com>
2015-05-15 12:08:56 +0800

    navbar shorter

--- app/uploaders/img_uploader.rb
+++ app/uploaders/img_uploader.rb
@@ -4,17 +4,16 @@ class ImgUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-  include Cloudinary::CarrierWave
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
+  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "upload/posts"
+    "uploads/posts"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
